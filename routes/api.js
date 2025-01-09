a91dec2c829f510db8f9504d7676ab82dc3d04b2
Rivera Corona Diego <73678986+DRCorona21@users.noreply.github.com>
2025-01-08 20:47:54 -0600

    Order, Tabla direcciones sin IDs

--- routes/api.js
+++ routes/api.js
@@ -8,7 +8,13 @@ router.get('/direccion', async(req, res) => {
     try {
         connection = await getConnection();
 
-        const result = await connection.execute(`SELECT * FROM DIRECCION`);
+        const result = await connection.execute(`
+            SELECT DIRECCION.ID, ESTADO, ALC_MUN, COLONIA, CP, CALLE, NO_INT, NO_EXT
+            FROM DIRECCION
+            JOIN ESTADO ON DIRECCION.ESTADO_ID = ESTADO.ID
+            JOIN ALC_MUN ON DIRECCION.ALC_MUN_ID = ALC_MUN.ID
+            ORDER BY DIRECCION.ID ASC
+        `);
         res.render('direccion', { data: result.rows });
     } catch (err) {
         console.error(err);
@@ -25,9 +31,11 @@ router.get('/direccion/:id', async(req, res) => {
         connection = await getConnection();
 
         const direccion_id = req.params.id;
-        const result = await connection.execute(
-            `SELECT * FROM DIRECCION WHERE ID = :direccion_id`, { direccion_id }
-        );
+        const result = await connection.execute(`
+            SELECT DIRECCION.ID, DIRECCION.ESTADO_ID, DIRECCION.ALC_MUN_ID, COLONIA, CP, CALLE, NO_INT, NO_EXT
+            FROM DIRECCION
+            WHERE DIRECCION.ID = :direccion_id
+        `, { direccion_id });
 
         if (result.rows.length > 0) {
             res.json(result.rows[0]);
